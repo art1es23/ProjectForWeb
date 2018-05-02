016d197c52f81ba3182ce6e930d8a895b53e142c
qmpwwsd <dreamwinamounts@gmail.com>
2018-05-02 22:08:02 +0300

    css+html(all)

--- MyProject/js/popup-menu.js
+++ MyProject/js/popup-menu.js
@@ -1,50 +1,13 @@
 'use strict';
 
-window.enableSetup = (function () {
-    let setupOpen = document.querySelector('.setup-open');
-    let setupWindow = document.querySelector('.setup');
-    let setupClose = setupWindow.querySelector('.setup-close');
-    let setupSubmit = setupWindow.querySelector('.setup-submit');
-    let onSetupClose;
+let setup = document.getElementById('form');
+let setupOpen = document.getElementById('btn');
+let setupClose = document.querySelector('.setup-close');
 
-    let closeSetupWindowByEnterKey = function (event) {
-        if (window.keydownHandler.isEnterKeyPressed(event)) {
-            closeSetupWindow();
-        }
-    };
+setupOpen.onClick = function () {
+    setup.style.display = "block";
+};
 
-    let closeSetupWindowByEscKey = function (event) {
-        if (window.keydownHandler.isEscKeyPressed(event)) {
-            closeSetupWindow();
-        }
-    };
-
-    let closeSetupWindow = function () {
-        setupClose.setAttribute('aria-pressed', true);
-        setupOpen.setAttribute('aria-pressed', false);
-        setupWindow.classList.add('hidden');
-        document.removeEventListener('keydown', closeSetupWindowByEscKey);
-        setupClose.removeEventListener('click', closeSetupWindow);
-        setupClose.removeEventListener('keydown', closeSetupWindowByEnterKey);
-        setupSubmit.removeEventListener('click', closeSetupWindow);
-        setupSubmit.removeEventListener('keydown', closeSetupWindowByEnterKey);
-
-        if (typeof onSetupClose === 'function') {
-            onSetupClose();
-        }
-    };
-
-    let showSetupWindow = function (callback) {
-        onSetupClose = callback;
-        setupOpen.setAttribute('aria-pressed', true);
-        setupClose.setAttribute('aria-pressed', false);
-        setupWindow.classList.remove('hidden');
-        document.addEventListener('keydown', closeSetupWindowByEscKey);
-        setupClose.addEventListener('click', closeSetupWindow);
-        setupClose.addEventListener('keydown', closeSetupWindowByEnterKey);
-        setupSubmit.addEventListener('click', closeSetupWindow);
-        setupSubmit.addEventListener('keydown', closeSetupWindowByEnterKey);
-    };
-
-    return showSetupWindow;
-})();
+setupClose.onClick = function () {
+    setup.style.display = "none";
+};
---
+++ MyProject/js/preloader.js
@@ -0,0 +1,13 @@
+'use strict';
+
+document.body.onload = function () {
+
+    setTimeout(function () {
+        let preloader = document.getElementById('page-preloader');
+
+        if (!preloader.classList.contains('done')) {
+            preloader.classList.add('done');
+        }
+    }, 1000)
+
+};
